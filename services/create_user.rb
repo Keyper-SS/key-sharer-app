64808593de106930c562954bde24860c155cbfd5
eduardo-salazar <esalazar922@gmail.com>
2016-05-06 08:35:19 +0800

    fix heroku vars

--- services/create_user.rb
+++ services/create_user.rb
@@ -6,6 +6,7 @@ class CreatedUser
   def self.call(email:,username:, password:)
     redirect = HTTP.post("#{ENV['api']}/users/",
                         json: {username: username, password: password, email: email})
+    puts redirect.headers.get("Location")
     response = HTTP.get(redirect.headers.get("Location")[0])
     response.code == 200 ? JSON.parse(response) : nil
   end
