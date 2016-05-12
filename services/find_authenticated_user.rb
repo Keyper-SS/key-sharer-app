0be270586843e94c1ddc35aebc70d91f470dd9af
vicky <lch82327@gmail.com>
2016-05-12 22:30:31 +0800

    add register services

--- services/find_authenticated_user.rb
+++ services/find_authenticated_user.rb
@@ -4,7 +4,7 @@ require 'http'
 class FindAuthenticatedUser
 
   def self.call(username:, password:)
-    response = HTTP.get("#{ENV['api']}/users/#{username}/authenticate",
+    response = HTTP.get("#{ENV['API_HOST']}/users/#{username}/authenticate",
                         params: {password: password})
     response.code == 200 ? JSON.parse({ :data => JSON.parse(response) }.to_json) : nil
   end
