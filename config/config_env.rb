c6ce714c4b1da4fc5b4c2ae072b17b9c2da9d4d0
yiweihuang <n1993212@gmail.com>
2016-05-12 22:34:31 +0800

    fix env

--- config/config_env.rb
+++ config/config_env.rb
@@ -1,4 +1,19 @@
-config_env :development,:test do
-  set 'api', 'http://localhost:3000/api/v1'
+config_env :development, :test do
+  set 'MSG_KEY', 'E8DlAZWnN0Sd2Oad0jjTQ9c4TnxRSSL9GhiKE3VQfng='
+
+  set 'API_HOST', 'http://key-sharer-api.herokuapp.com/api/v1'
+  set 'APP_HOST', 'http://localhost:9292'
 end
 
+config_env :production do
+  set 'MSG_KEY', 'fDJvpRnepnLFYu/Xnp/g5d/bWI641RILd+UOeFAF4kM='
+
+  set 'API_HOST', 'http://key-sharer-api.herokuapp.com/api/v1'
+  set 'APP_HOST', 'https://key-sharer.herokuapp.com/'
+end
+
+config_env do
+  set 'SENDGRID_DOMAIN', 'heroku.com'
+  set 'SENDGRID_USERNAME', 'app50405591@heroku.com'
+  set 'SENDGRID_PASSWORD', 'gus5ozkf2922'
+end
