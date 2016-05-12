28f410a4c8c9afc16b88eddec36f0211ee144e97
vicky <lch82327@gmail.com>
2016-05-12 22:29:15 +0800

    add session

--- controllers/base.rb
+++ controllers/base.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
-require 'sinatra/flash'
-# Basec class for ConfigShare Web Application
+
+# Base class for ConfigShare Web Application
 class KeySharerApp < Sinatra::Base
-  use Rack::Session::Cookie, expire_after: 2_592_000 # One month in seconds
-  register Sinatra::Flash
+  use Rack::Session::Cookie, coder: CookieEncoder.new,
+                             let_coder_handle_secure_encoding: true
+
   set :views, File.expand_path('../../views', __FILE__)
-  set :public_folder, File.dirname(__FILE__) + '/../public'
+
   before do
     @current_user = session[:current_user]
   end
