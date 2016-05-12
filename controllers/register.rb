f36f96243a5844fd409229911bc02a90205a0b6f
vicky <lch82327@gmail.com>
2016-05-12 22:34:41 +0800

    add register controller

--- controllers/register.rb
+++ controllers/register.rb
@@ -1,22 +1,44 @@
 require 'sinatra'
 
-class ShareConfigurationsApp < Sinatra::Base
+class KeySharerApp < Sinatra::Base
+  get '/register' do
+    slim :home
+  end
+
   post '/register' do
-    email = params[:email]
-    username = params[:username]
-    password = params[:password]
-
-    @current_user = CreatedUser.call(email: email,
-      username: username, password: password)
-
-    if @current_user
-      session[:current_user] = @current_user
-      puts @current_user
-      flash[:info] = "User #{@current_user['data']['attributes']['username']} created"
-      slim :home
-    else
-      flash[:error] = "Error creating user."
-      slim :home
+    begin
+      puts params[:username]
+      puts params[:email]
+      EmailRegistrationVerification.call(
+        username: params[:username],
+        email: params[:email])
+      redirect '/'
+    rescue => e
+      puts "FAIL EMAIL: #{e}"
+      redirect '/register'
     end
+  end
+
+  get '/register/:token_secure/verify' do
+    @token_secure = params[:token_secure]
+    @new_user = SecureMessage.decrypt(@token_secure)
+
+    slim :register_confirm
+  end
+
+  post '/register/:token_secure/verify' do
+    redirect "/register/#{params[:token_secure]}/verify" unless
+      (params[:password] == params[:password_confirm]) &&
+      !params[:password].empty?
+
+    new_user = SecureMessage.decrypt(params[:token_secure])
+
+    result = CreateVerifiedUser.call(
+      username: new_user[:username],
+      email: new_user[:email],
+      password: params[:password])
+
+    puts "RESULT: #{result}"
+    result ? redirect('/login') : redirect('/register')
   end
 end
