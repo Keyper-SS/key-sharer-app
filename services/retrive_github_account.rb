adf4b5eab08dd2d651d62ab1d71efc9a00676132
eduardo-salazar <esalazar922@gmail.com>
2016-06-16 16:44:34 +0800

    Signing code message

--- services/retrive_github_account.rb
+++ services/retrive_github_account.rb
@@ -3,6 +3,7 @@ require 'http'
 # Returns an authenticated user, or nil
 class RetrieveGithubAccount
   def self.call(code)
+  	code = SecureMessage.sign(code)
     response = HTTP.headers(accept: 'application/json')
                    .get("#{ENV['API_HOST']}/github_account?code=#{code}")
     response.code == 200 ? JSON.parse({ :data => JSON.parse(response) }.to_json) : nil
