b0d9704e48be015402eb81f155d6c70d43492d92
eduardo-salazar <esalazar922@gmail.com>
2016-06-16 16:21:59 +0800

    Sending signed message when creating user and authenticating

--- services/find_authenticated_user.rb
+++ services/find_authenticated_user.rb
@@ -2,13 +2,13 @@ require 'http'
 
 # Returns an authenticated user, or nil
 class FindAuthenticatedUser
-
   def self.call(username:, password:)
+  	signed_request = SecureMessage.sign({
+              	username: username,
+              	password: password
+              })
     response = HTTP.post("#{ENV['API_HOST']}/users/authenticate",
-                        json: {
-                        	username: username,
-                        	password: password
-                        })
+    										body: signed_request)
     response.code == 200 ? JSON.parse({ :data => JSON.parse(response) }.to_json) : nil
   end
 
